afbdc71eeb6bbb4b8fb588ad4107efff204c1dd0
Shyamsundar Shrestha <ss.stha00@gmail.com>
2024-04-06 02:32:46 +1030

    starting fresh

--- ft_printf.c
+++ ft_printf.c
@@ -5,68 +5,13 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sshresth <sshresth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/02 01:06:12 by sshresth          #+#    #+#             */
-/*   Updated: 2024/04/03 22:43:29 by sshresth         ###   ########.fr       */
+/*   Created: 2024/04/06 02:30:52 by sshresth          #+#    #+#             */
+/*   Updated: 2024/04/06 02:31:47 by sshresth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf(const char *str, ...)
+int	ft_printf(const char *format, ...)
 {
-	int		index;
-	int		length;
-	va_list	args;
-
-	index = 0;
-	length = 0;
-	va_start(args, str);
-	while (str[index] != '\0')
-	{
-		if (str[index] == '%')
-		{
-			length = ft_apply(args, str[index + 1]);
-			index++;
-		}
-		else
-		{
-			write(1, &str[index], 1);
-			index++;
-		}
-		index++;
-	}
-	va_end(args);
-	return (length);
-}
-
-int	ft_apply(va_list args, const char format)
-{
-	int	length;
-	
-	length = 0;
-	if (format == 'c')
-		length = ft_print_c(va_arg(args, int));
-	else if (format == 's')
-		length = ft_print_s(va_arg(args, char *));
-	else if (format == 'p')
-		length = ft_print_p((unsigned long)va_arg(args, char *));
-	else if (format == 'd' || format == 'i')
-		length = ft_print_di(va_arg(args, int));
-	else if (format == 'u')
-		length = ft_print_u(va_arg(args, unsigned int));
-	else if (format == 'x')
-		length = ft_print_x(va_arg(args, unsigned int), 1);
-	else if (format == 'X')
-		length = ft_print_x(va_arg(args, unsigned int), 2);
-	else if (format == '%')
-	{
-		length = write(1, "%", 1);
-	}
-	return (length);
-}
-
-int	ft_print_c(int c)
-{
-	write(1, &c, 1);
-	return (1);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -5,23 +5,11 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sshresth <sshresth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/02 01:05:15 by sshresth          #+#    #+#             */
-/*   Updated: 2024/04/03 20:41:21 by sshresth         ###   ########.fr       */
+/*   Created: 2024/04/06 02:30:49 by sshresth          #+#    #+#             */
+/*   Updated: 2024/04/06 02:32:24 by sshresth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
 
 #include "libft/libft.h"
 
 int	ft_printf(const char *format, ...);
-int	ft_apply(va_list args, const char format);
-int	ft_print_c(int c);
-int ft_print_s(char *str);
-int	ft_print_p(unsigned	long int num);
-int	ft_print_di(int	num);
-int	ft_print_u(unsigned int num);
-int	ft_print_x(unsigned int num, int flag);
-
-#endif
